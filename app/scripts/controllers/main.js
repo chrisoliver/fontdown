0096461803def141174252a566820d82548533d6
Chris Oliver <chrisolly@gmail.com>
2017-04-03 22:17:19 +0100

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fontdownApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fontdownApp
+ */
+angular.module('fontdownApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
